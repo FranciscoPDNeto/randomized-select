ede530eb7648535058911f103901152473246d91
Alexandre Cesar <acmorcego@gmail.com>
2019-09-22 12:51:11 -0300

    fix pivot name

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -39,15 +39,15 @@ int randomizedPartition(std::vector<int>& setA,
 int partition(std::vector<int>& setA,
               const int partBeginning,
               const int partEnding) {
-  int x = setA[partEnding];
-  int pivotIndex = partBeginning - 1;
+  int pivot = setA[partEnding];
+  int i = partBeginning - 1;
   for (int j = partBeginning; j < partEnding; ++j) {
-    if (setA[j] <= x) {
-      pivotIndex += 1;
-      std::swap(setA[pivotIndex], setA[j]);
+    if (setA[j] <= pivot) {
+      i += 1;
+      std::swap(setA[i], setA[j]);
     }
   }
 
-  std::swap(setA[pivotIndex + 1], setA[partEnding]);
-  return pivotIndex + 1;
+  std::swap(setA[i + 1], setA[partEnding]);
+  return i + 1;
 }
