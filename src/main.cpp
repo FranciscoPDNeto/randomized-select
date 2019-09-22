650d2db6f7cbe695d2286b8141bb0d3a1a384211
Francisco Dias <franciscopdneto@gmail.com>
2019-09-22 16:37:05 -0300

    change rand vector generator. number of args main.

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,21 @@
 #include <iostream>
 #include <limits>
 #include <random>
-#include <set>
+#include <unordered_set>
 #include <string>
 
 #include "randomizedSelect.h"
 
 int main(int argc, char** argv) {
-  if (argc != 4)
+  if (argc != 5)
     return -1;
 
   unsigned int setSize = std::stoul(argv[1]);
   const unsigned int selectionCut = std::stoul(argv[2]);
-  unsigned int numberOfThreads = std::stoul(argv[3]);
+  const std::string type(argv[3]);
+  unsigned int numberOfThreads = std::stoul(argv[4]);
 
+  /*
   // O vetor deve ser elementos únicos, seria melhor usar o set,
   // mas teria que colocar os elementos aleatórios nele.
   std::vector<int> setA;
@@ -24,12 +26,33 @@ int main(int argc, char** argv) {
   std::mt19937 engine;  // Mersenne twister MT19937
   auto generator = std::bind(distribution, engine);
   std::generate_n(setA.begin(), setSize, generator);
+  */
+  std::unordered_set<int> setA;
+  setA.reserve(setSize);
+  std::random_device rd;
+  std::mt19937 engine(rd());  // Mersenne twister MT19937
+  std::uniform_int_distribution<int> distribution(
+    0, std::numeric_limits<int>::max());
+  
+  while (setA.size() != setSize) {
+    setA.insert(distribution(engine));
+  }
+
+  std::vector<int> vecA(setA.begin(), setA.end());
+  setA.~unordered_set();
+  std::cout << "Vector generated: ";
+  for (const int a : vecA) {
+    std::cout << a << " ";
+  }
+  std::cout << std::endl;
 
   const auto startTime = std::chrono::high_resolution_clock::now();
 
-  randomizedSelect(setA, 0, setSize - 1, rand() % setSize + 1, numberOfThreads);
+  const int value = randomizedSelect(vecA, 0, setSize - 1, selectionCut, numberOfThreads);
 
   const auto endTime = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Result:" << value << std::endl;
 
   // TODO Imprimir ith número em A, o tempo de execução em segundos, e os
   // valores de A.
